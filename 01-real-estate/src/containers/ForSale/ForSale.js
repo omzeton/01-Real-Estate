76dd22a4f930d7d1e71171c6c58597dd5f20e477
omzeton <adamr6561@gmail.com>
2019-03-12 09:56:56 +0100

    Install react-thunk package

--- 01-real-estate/src/containers/ForSale/ForSale.js
+++ 01-real-estate/src/containers/ForSale/ForSale.js
@@ -1,14 +1,59 @@
 import React, { Component } from 'react';
 import Sort from '../../components/Sort/Sort';
+import axios from 'axios';
+import Result from '../../components/Result/Result';
 import './ForSale.css';
 
 class ForSale extends Component {
+	state = {
+		samples : null,
+	}
+
+
+	componentDidMount () {
+		axios.get('https://real-estate-d9a1e.firebaseio.com/examples.json')
+			.then(response => {
+				this.setState({samples: response.data});
+			});
+	}
 	
 	render() {
 
+		let results = null;
+		let maxAmount = null;
+		let split = null;
+
+		if (this.state.samples) {
+			results = this.state.samples.map(result => {
+				if (result.forSale) {
+					return <Result 
+						key={result.id}
+						img={result.img} 
+						name={result.name}
+						price={result.price} 
+						town={result.town}
+						beds={result.beds}
+						type={result.type}
+						id={result.id}
+						status={"For Sale"}
+						/>
+				} else {
+					return null
+				}
+			});
+
+			maxAmount = this.state.samples.includes({"forSale": true}).length;
+			if(maxAmount >= this.props.ctr) {
+				split = this.props.ctr;
+			} else {
+				split = maxAmount;
+			}
+		}
+
 		return (
 			<div className="ForSale">
-				<Sort />
+				<Sort split={split} maxAmount={maxAmount}/>
+				{ results }
 			</div>
 		);
 	}
--- 01-real-estate/src/store/reducer.js
+++ 01-real-estate/src/store/reducer.js
@@ -1,15 +1,48 @@
+import { applyMiddleware, createStore } from 'redux';
+import axios from 'axios';
+import thunk from 'redux-thunk';
+
 const initalState = {
 	// this is the global store
+	fetching: false,
+	fetched: false,
+	samples: null,
 	limit: 5
 }
 
 const reducer = (state = initalState, action) => {
-	if(action.type === 'INCREMENT') {
-		return {
-			limit: state.counter + 1
+	switch (action.type) {
+		case "FETCH_SAMPLES_START": {
+			return {...state, fetching: true}
+			break;
+		}
+		case "FETCH_SAMPLES_ERROR": {
+			return {...state, fetching: false, error: action.payload}
+			break;
+		}
+		case "RECEIVE_SAMPLES": {
+			return {...state,
+				fetcing:false,
+				fetched: true,
+				samples: action.payload
+			}
 		}
 	}
 	return state;
 }
+
+const middleware = applyMiddleware(thunk);
+const store = createStore(reducer, middleware);
+
+store.dispatch((dispatch) => {
+	dispatch({type: "FETCH_SAMPLES_START"})
+	axios.get('https://real-estate-d9a1e.firebaseio.com/examples.json')
+		.then(response => {
+			dispatch({type: "RECEIVE_SAMPLES", payload: response.data})
+		})
+		.catch((err) => {
+			dispatch({type: "FETCH_SAMPLES_ERROR", payload: err})
+		})
+});	
 
 export default reducer;
