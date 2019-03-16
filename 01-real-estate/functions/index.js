77e3d498773ac554368ee1591afeb20e4b61d9a8
omzeton <adamr6561@gmail.com>
2019-03-16 11:39:43 +0100

    Create Firebase Cloud Functions

--- 01-real-estate/functions/index.js
+++ 01-real-estate/functions/index.js
@@ -1,8 +1,46 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
+const gcs = require('@google-cloud/storage');
+const os = require('os');
+const path = require('path');
+const spawn = require('child-process-promise').spawn;
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.onFileFinalize= functions.storage.object().onFinalize(event => {
+    const object = event.data;
+    const bucket = object.bucket;
+    const contentType = object.contentType;
+    const filePath = object.name;
+    console.log('File change detected, function execution started');
+
+    if (object.resourceState === 'not_exists') {
+        console.log('We deleted a file, exit...');
+        return;
+    }
+
+    if (path.basename(filePath).startsWith('resized-')) {
+        console.log('We already renamed that file!');
+        return;
+    }
+
+    const destBucket = gcs.bucket(bucket);
+    const tmpFilePath = path.join(os.tmpdir(), path.basename(filePath));
+    const metadata = { contentType: contentType };
+    return destBucket.file(filePath).download({
+        destination: tmpFilePath
+    }).then(() => {
+        return spawn('convert', [tmpFilePath, '-resize', '500x500', tmpFilePath]);
+    }).then(() => {
+        return destBucket.upload(tmpFilePath, {
+            destination: 'resized-' + path.basename(filePath),
+            metadata: metadata
+        })
+    });
+});
+
+exports.fileDeleted = functions.storage.object().onDelete(event => {
+	console.log("This file was deleted");
+	console.log(event);
+	return;
+});
