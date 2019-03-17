cd9f6a54dd924ff9dc27b864f96a20c6a3bc5735
omzeton <adamr6561@gmail.com>
2019-03-17 12:10:01 +0100

    Error - cannot read property of bucket in Firebase Cloud Function for renaming file

--- 01-real-estate/functions/index.js
+++ 01-real-estate/functions/index.js
@@ -1,47 +1,58 @@
-const functions = require("firebase-functions");
-const gcs = require('@google-cloud/storage');
-const os = require('os');
-const path = require('path');
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp()
 const spawn = require('child-process-promise').spawn;
+const path = require('path');
+const os = require('os');
+const fs = require('fs');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 
-// exports.onFileFinalize= functions.storage.object().onFinalize(event => {
-//     const object = event.data;
-//     const bucket = object.bucket;
-//     const contentType = object.contentType;
-//     const filePath = object.name;
-//     console.log('File change detected, function execution started');
+exports.delete = functions.storage.object().onDelete(event => {
+	console.log('onDelete has been fired!');
+	console.log(event);
+	return;
+});
 
-//     if (object.resourceState === 'not_exists') {
-//         console.log('We deleted a file, exit...');
-//         return;
-//     }
+exports.finalize = functions.storage.object().onFinalize(event => {
+	console.log('onFinalize has been fired!');
+	console.log(event);
+	return;
+});
 
-//     if (path.basename(filePath).startsWith('resized-')) {
-//         console.log('We already renamed that file!');
-//         return;
-//     }
+// Out of all 4: onArchive, onDelete, onFinalize and onMetadataUpdate.
+// Only onDelete and onFinalize fired.
 
-//     const destBucket = gcs.bucket(bucket);
-//     const tmpFilePath = path.join(os.tmpdir(), path.basename(filePath));
-//     const metadata = { contentType: contentType };
-//     return destBucket.file(filePath).download({
-//         destination: tmpFilePath
-//     }).then(() => {
-//         return spawn('convert', [tmpFilePath, '-resize', '500x500', tmpFilePath]);
-//     }).then(() => {
-//         return destBucket.upload(tmpFilePath, {
-//             destination: 'resized-' + path.basename(filePath),
-//             metadata: metadata
-//         })
-//     });
-// });
+exports.changeName = functions.storage.object().onFinalize(event => {
+	const object = event.data;
+    const bucket = object.bucket;
+    const contentType = object.contentType;
+    const filePath = object.name;
+    console.log(event);
+    console.log(object);
+    console.log(bucket);
+	console.log('File change detected, function execution started');
 
-// exports.fileDeleted = functions.storage.object().onDelete(event => {
-// 	console.log("This file was deleted");
-// 	console.log(event);
-// 	return;
-// });
+
+	if (path.basename(filePath).startsWith('renamed-')) {
+        console.log('We already renamed that file!');
+        return;
+    }
+
+	
+	const destBucket = admin.storage().bucket(bucket); // Here you can change the bucket of the uploaded file
+	const tmpFilePath = path.join(os.tmpdir(), path.basename(filePath));
+	const metadata = { contentType: contentType };
+	return destBucket.file(filePath).download({
+		destination: tempFilePath
+	}).then(() => {
+		// this is where all the operations take place
+		return destBucket.upload(tmpFilePath, {
+		    destination: 'renamed-' + path.basename(filePath),
+		    metadata: metadata
+		})
+	});
+
+});
--- 01-real-estate/src/containers/ListYourProperty/ListYourProperty.js
+++ 01-real-estate/src/containers/ListYourProperty/ListYourProperty.js
@@ -4,18 +4,96 @@ import './ListYourProperty.css';
 class ListYourProperty extends Component {
 
 	state = {
-		selectedFile: null
+		saleRent: 'forSale',
+		img: '',
+		info: '',
+		price: '',
+		id: '',
+		type: 'house',
+		town: '',
+		name: '',
+		beds: '1+',
+		object: {
+			"forSale" : undefined,
+			"img" : undefined,
+			"info" : undefined,
+			"price" : undefined,
+			"id" : undefined,
+			"type" : undefined,
+			"town" : undefined,
+			"toRent" : undefined,
+			"beds" : undefined,
+			"name" : undefined
+		}
 	}
 
-	fileSelectedHandler = event => {
+	getRandomInt( min, max ) {
+		return Math.floor( Math.random() * ( max - min + 1 ) ) + min;
+	}
+
+	nameHandler = (e) => {
+		this.setState({name: e.target.value });
+	}
+	townHandler = (e) => {
+		this.setState({town: e.target.value });
+	}
+	bedsHandler = (e) => {
+		this.setState({beds: e.target.value });
+	}
+	priceHandler = (e) => {
+		this.setState({price: e.target.value });
+		console.log("state updated");
+	}
+	saleRentHandler = (e) => {
+		this.setState({saleRent: e.target.value});
+	}
+	typeHandler = (e) => {
+		this.setState({type: e.target.value});
+	}
+	infoHandler = (e) => {
+		this.setState({info: e.target.value});
+	}
+	imgHandler = (e) => {
+		this.setState({img: e.target.files[0]});
+	}
+
+
+	uploadHanlder = () => {
+		let name = this.state.name,
+			img = this.state.img,
+			info = this.state.info,
+			price = this.state.price,
+			type = this.state.type,
+			town = this.state.town,
+			saleRent = this.state.saleRent,
+			beds = this.state.beds,
+			id = this.getRandomInt(0,99999999);
+		let forSale = Boolean,
+			toRent = Boolean;
+
+		if ( saleRent === "forSale" ) {
+			forSale = true;
+			toRent = false;
+		} else if ( saleRent === 'toRent') {
+			forSale = false;
+			toRent = true;
+		}
 		this.setState({
-			selectedFile: event.target.files[0]
+			object: {
+				"forSale" : forSale,
+				"img" : img,
+				"info" : info,
+				"price" : price,
+				"id" : id,
+				"type" : type,
+				"town" : town,
+				"toRent" : toRent,
+				"beds" : beds,
+				"name" : name
+			}
 		});
+		console.log(this.state.object);
 	}
-
-	// fileUploadHandler = () => {
-	// 	axios.post('')
-	// }
 
 	render() {
 		return (
@@ -33,32 +111,45 @@ class ListYourProperty extends Component {
 										<h2>List your property</h2>
 									</div>
 									<div className="List__FormContainer--input">
+
 										<h3>Title of property</h3>
-										<input type="text" name="name" placeholder="Name" ref={(input) => {this.name = input}}/>
+										<input type="text" placeholder="Name" value={this.state.name} onChange={this.nameHandler}/>
 										<h3>Town</h3>
-										<input type="text" name="town" placeholder="Town" ref={(input) => {this.town = input}}/>
+										<input type="text" placeholder="Town" value={this.state.town} onChange={this.townHandler}/>
+
 										<h3>Number of beds</h3>
-										<select name="beds" ref={(select) => {this.beds = select}}>
+										<select value={this.state.beds} onChange={this.bedsHandler}>
 											    <option value="1+">1+</option>
 											    <option value="2+">2+</option>
 											    <option value="3+">3+</option>
 											    <option value="4+">4+</option>
 										</select>
+
 										<h3>Price in dollars</h3>
-										<input type="number" name="price" placeholder="Price" ref={(input) => {this.price = input}}/>
+										<input type="number" value={this.state.price} placeholder="Price" onChange={this.priceHandler}/>
+
+										<h3>For Sale / To Rent</h3>
+										<select value={this.state.saleRent} onChange={this.saleRentHandler}>
+											    <option value="forSale">For Sale</option>
+											    <option value="toRent">To Rent</option>
+										</select>
+
 										<h3>Type of property</h3>
-										  <select name="type" ref={(select) => {this.type = select}}>
+										  <select value={this.state.type} onChange={this.typeHandler}>
 											    <option value="house">House</option>
 											    <option value="bungalow">Bungalow</option>
 											    <option value="apartment">Apartment</option>
 											    <option value="commercial property">Commercial Property</option>
 											    <option value="industrial property">Industrial Property</option>
 										  </select>
+
 										<h3>Short description</h3>
-										<input type="text" name="info" placeholder="info" ref={(input) => {this.info = input}}/>
+										<input type="text" value={this.state.info} placeholder="info" onChange={this.infoHandler}/>
+
 										<h3>Image of your property</h3>
-										<input type="file" onChange={this.fileSelectedHandler}/>
-							 	  		<input type="submit" value="Submit" onClick={this.fileUploadHandler}/>
+										<input type="file" ref={(input) => {this.image = input}} onChange={this.imgHandler}/>
+
+							 	  		<input type="submit" value="Submit" onClick={this.uploadHanlder}/>
 									</div>
 							</div>
 							<div></div>
