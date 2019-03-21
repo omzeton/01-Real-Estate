4527285c81ecaa1c812dda07c1917506fa40a1f4
omzeton <adamr6561@gmail.com>
2019-03-21 21:56:16 +0100

    Set up environment for adding media-queires to whole app

--- 01-real-estate/src/components/Objectives/Objectives.js
+++ 01-real-estate/src/components/Objectives/Objectives.js
@@ -20,12 +20,17 @@ class Objectives extends Component {
 							<li>Loader in form </li>
 							<li>Cloud function - if file has the same as before rename to sth unique.</li>
 							<hr />
+							<li>CSSTransitions</li>
 							<li>Media Queries - App has to look good on smaller devices.</li>
-							<li>Better naming / Better folder structure / Code improvements / Check if some containers are still in components folder</li>
-							<hr />
-							<li>润色 on styling.</li>
+							<li>润色: Better folder structure / Code improvements / Check if some containers are still in components folder</li>
 						</ul>
-					</div>		
+						<div className="queries">
+							<div className="queries__container"><div className="red"></div><h2>&#8734; - 768</h2></div>
+							<div className="queries__container"><div className="green"></div><h2>768 - 992</h2></div>
+							<div className="queries__container"><div className="blue"></div><h2>992 - 1200</h2></div>
+							<div className="queries__container"><div className="orange"></div><h2>1200 - &#8734;</h2></div>
+						</div>
+					</div>
 				</div>
 			);
 		}
