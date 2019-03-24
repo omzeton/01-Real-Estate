e5be2ca3a5da5797edadfa87aa358ae7926e2a8a
omzeton <adamr6561@gmail.com>
2019-03-24 10:37:40 +0100

    Style links in responsive nav

--- 01-real-estate/src/components/TopMenu/TopMenu.js
+++ 01-real-estate/src/components/TopMenu/TopMenu.js
@@ -49,7 +49,7 @@ class TopMenu extends Component {
 			}
 		});
 
-		const style = this.state.open ? { height: '100vh' } : { height: '4em' }
+		const style = this.state.open ? { height: '50vh' } : { height: '4em' }
 		const burgerStyle = this.state.open ? ['hamburger', 'hamburger--squeeze', 'is-active'].join(' ') : ['hamburger', 'hamburger--squeeze'].join(' ');
 
 		const smallMenu = <div className="Responsive-TopMenu" style={style}>
